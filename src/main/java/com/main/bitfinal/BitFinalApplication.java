c151089947361241c90cdfe9fcc1ee0b6601cf95
choibin12 <unmbb123@naver.com>
2022-12-26 12:29:24 +0900

    go!

---
+++ src/main/java/com/main/bitfinal/BitFinalApplication.java
@@ -0,0 +1,15 @@
+package com.main.bitfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitFinalApplication.class , args);
+        System.out.println("!1");
+        System.out.println("!1");
+    }
+
+        }
